613971b592999053d0d9c5c813c9aac6c3ba02f3
Nicholas Saney <nsaney@gmail.com>
2019-10-05 14:35:32 -0400

    Updated wotd page webscraping to adjust for dictionary dot com UI changes.

--- root/screen/wotd.js
+++ root/screen/wotd.js
@@ -4,19 +4,19 @@ __sb.fn.__addScreen('wotd', function wotd(self) {
     self.getAjaxSettings = function getAjaxSettings() {
         return __sb.Screen.proxyAjaxSettings(self, {
             method: 'GET',
-            url: 'https://www.dictionary.com/wordoftheday/'
+            url: 'https://www.dictionary.com/e/word-of-the-day/'
         });
     };
     self.parseRawData = function parseRawData(rawData) {
         var doc = document.implementation.createHTMLDocument("dummy");
         doc.documentElement.innerHTML = rawData;
-        var img = doc.querySelectorAll('.wotd-today a[href*="dictionary.com/browse"] img')[0];
-        var data = {
-            imageAlt: img.alt || '',
-            imageSrc: img.src,
-            imageSrcUrl: 'url("' + img.src + '")'
-        };
-//        console.log(data);
+        var wotdItem = doc.querySelector('li.wotd-item') || {};
+        var wotdPronDiv = doc.querySelector('div.wotd-item__definition__pronunciation') || {};
+        var wotdDefDiv = doc.querySelector('div.wotd-item__definition__text') || {};
+        var data = wotdItem.dataset || {};
+        data.pronunciationHtml = (wotdPronDiv.innerHTML || '').trim();
+        data.definitionText = (wotdDefDiv.innerText || '').trim();
+        //console.log(data);
         return data;
     };
 });
